079e26cf0abfd63486027b99c211c42ed379c8d5
leifriksheim <riksheim.leif@gmail.com>
2018-07-23 10:50:27 +0200

    Add new intro text

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -22,23 +22,26 @@ function Intro({ close, data }) {
       <IntroMain>
         <Primitives.Heading.H1>Bruksendring</Primitives.Heading.H1>
         <Primitives.Paragraphs.Lead>
-        Vil du endre hva du bruker et rom i boligen din til? Finn ut om du må sende byggesøknad til kommunen, eller om du kan slippe å søke. Du får også vite hvilke byggeregler du må følge.<br /><br />Veiviseren gjelder for deg som ønsker å endre på ett enkelt rom uten å flytte vegger eller dører.
+          Vil du endre hva du bruker et rom i boligen din til? Finn ut om du må sende byggesøknad
+          til kommunen, eller om du kan slippe å søke. Du får også vite hvilke byggeregler du må
+          følge.<br />
+          <br />Veiviseren gjelder for deg som ønsker å endre på ett enkelt rom uten å flytte vegger
+          eller dører.
         </Primitives.Paragraphs.Lead>
         <section>
           <div>
             <Primitives.Heading.H2>Før du begynner</Primitives.Heading.H2>
             <Primitives.Paragraphs.P>
-              Før du tar veiviseren bør du spørre kommunen om å få nyeste godkjente plantegning av boligen din. Denne viser hva du har lov å bruke rommene til. Skaff også kommunale dokumenter som reguleringsplan, kommuneplanens arealdel eller situasjonskart. Der kan det finnes begrensninger som:
+              Før du tar veiviseren bør du kontakte kommunen og få tak i siste godkjente plantegning
+              av boligen din. Plantegningen viser hva du har lov å bruke rommene i boligen din til.
+              Du vil få bruk for denne informasjonen i veiviseren.
             </Primitives.Paragraphs.P>
-            <ul>
-              <li>flom- og skredsområde</li>
-              <li>avstand til kraftlinjer</li>
-              <li>fredet eller vernestatus</li>
-            </ul>
             <br />
             <Primitives.Heading.H2>Start nå og fullfør senere</Primitives.Heading.H2>
             <Primitives.Paragraphs.P>
-              Det er du som er ansvarlig for at alle lover og regler blir fulgt når du bygger. Er det noen spørsmål du er usikker på underveis? Du kan ta en pause og fortsette senere. Nettleseren husker hvor du var.
+              Det er du som er ansvarlig for at alle lover og regler blir fulgt når du bygger. Er
+              det noen spørsmål du er usikker på underveis? Du kan ta en pause og fortsette senere.
+              Nettleseren husker hvor du var.
             </Primitives.Paragraphs.P>
           </div>
         </section>
